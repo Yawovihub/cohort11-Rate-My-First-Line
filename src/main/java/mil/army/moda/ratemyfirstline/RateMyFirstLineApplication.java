32d75c976791b5ebd5ec7242917c2cc24f5085df
yawovi degbe <yawovi.p.degbe.mil@army.mil>
2026-04-20 15:48:58 -0500

    first commit

---
+++ src/main/java/mil/army/moda/ratemyfirstline/RateMyFirstLineApplication.java
@@ -0,0 +1,13 @@
+package mil.army.moda.ratemyfirstline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateMyFirstLineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateMyFirstLineApplication.class, args);
+    }
+
+}
