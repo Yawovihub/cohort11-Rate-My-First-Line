0dd1262579ec98d56829427592c9d66ea366a94f
wyatt.t.putnam.mil@army.mil <wyatt.t.putnam.mil@army.mil>
2026-04-24 16:20:22 -0500

    Passing tests

--- frontend/src/__tests__/SubmitLeader.test.tsx
+++ frontend/src/__tests__/SubmitLeader.test.tsx
@@ -6,11 +6,11 @@ describe('Leader Submission Page', () => {
         render(<SubmitLeader/>);
         expect(screen.getByRole('heading', {name: /submit new leader/i})).toBeInTheDocument();
         screen.logTestingPlaygroundURL();
-        expect(screen.getByLabelText(/name/i)).toBeInTheDocument();
-        expect(screen.getByLabelText(/description/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/First Name/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/Last Name/i)).toBeInTheDocument();
         expect(screen.getByLabelText(/job title/i)).toBeInTheDocument();
-        expect(screen.getByRole('textbox', { name: /name/i})).toBeInTheDocument();
-        expect(screen.getByRole('textbox', { name: /description/i})).toBeInTheDocument();
+        expect(screen.getByRole('textbox', { name: /first name/i})).toBeInTheDocument();
+        expect(screen.getByRole('textbox', { name: /last name/i})).toBeInTheDocument();
         expect(screen.getByRole('textbox', { name: /job title/i})).toBeInTheDocument();
         expect(screen.getByRole('button', { name: /reset/i})).toBeInTheDocument();
         expect(screen.getByRole('button', { name: /submit/i})).toBeInTheDocument();
