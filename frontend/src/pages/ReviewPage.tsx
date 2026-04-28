556b1c569d61179474f2975593adc4c857c70074
Chuma Humphrey <chumphrey17c@gmail.com>
2026-04-28 11:52:07 -0500

    needed to create a ReviewPage.tsx which holds the ReviewForm for separation of concerns and address missing props

---
+++ frontend/src/pages/ReviewPage.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import ReviewForm from '../components/ReviewForm.tsx';
+
+export const ReviewPage = () => {
+    const [isFormOpen, setIsFormOpen] = useState(false);
+
+
+    // Grabbed this from to-do app/Gemini, we need to separate concerns here so that our App.jsx stays clean (see to-do TaskPage.tsx)
+    return(
+        <>
+            <h1>This is the Review Page</h1>
+
+            <button onClick={() => setIsFormOpen(true)}>
+                {/*Leave a Review*/}
+            </button>
+
+            <ReviewForm
+                isOpen={isFormOpen}
+                onClose={() => setIsFormOpen(false)}
+            />
+
+            <h2 className={'bold'}> Review Box/Card Here?</h2>
+            <ul id={'list'}>
+                <li>No Reviews</li>
+            </ul>
+        </>
+    )
+}
